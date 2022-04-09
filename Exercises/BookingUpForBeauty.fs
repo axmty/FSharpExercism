61ea50bcb24f3f394e8059205adf767bbc2ca795
axmty <alexandre.metgy@gmail.com>
2022-04-09 23:02:53 +0200

    Add 3 exercises.

---
+++ Exercises/BookingUpForBeauty.fs
@@ -0,0 +1,16 @@
+module BookingUpForBeauty
+
+open System
+
+let schedule = DateTime.Parse
+
+let hasPassed (appointmentDate: DateTime) : bool = DateTime.Now > appointmentDate
+
+let isAfternoonAppointment (appointmentDate: DateTime) : bool =
+  let hour = appointmentDate.Hour
+  hour >= 12 && hour < 18
+
+let description (appointmentDate: DateTime) : string =
+  sprintf "You have an appointment on %A." appointmentDate
+
+let anniversaryDate () : DateTime = new DateTime(DateTime.Now.Year, 9, 15)
