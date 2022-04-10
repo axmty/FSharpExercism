aafe743c690f5f31b77e10721c1bd95fc4ec7e1d
axmty <alexandre.metgy@gmail.com>
2022-04-10 17:04:07 +0200

    Add 4 exercises.

---
+++ Exercises/GradeSchool.fs
@@ -0,0 +1,24 @@
+module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School) : School =
+  let newList =
+    match school |> Map.tryFind grade with
+    | Some old -> student :: old
+    | None -> List.singleton student
+
+  Map.add grade newList school
+
+let roster (school: School) : string list =
+  school
+  |> Map.toList
+  |> List.sortBy fst
+  |> List.collect (snd >> List.sort)
+
+let grade (number: int) (school: School) : string list =
+  match school |> Map.tryFind number with
+  | Some students -> students
+  | None -> []
--- Exercises/QueenAttack.fs
+++ Exercises/QueenAttack.fs
@@ -4,8 +4,7 @@ let create (position: int * int) =
   match position with
   | (x, y) -> x >= 0 && x < 8 && y >= 0 && y < 8
 
-let canAttack (queen1: int * int) (queen2: int * int) =
-  match queen1, queen2 with
-  | (x1, _), (x2, _) when x1 = x2 -> true
-  | (_, y1), (_, y2) when y1 = y2 -> true
-  | (x1, y1), (x2, y2) -> abs (y2 - y1) = abs (x2 - x1)
+let canAttack (x1, y1) (x2, y2) =
+  x1 = x2
+  || y1 = y2
+  || (y2 - y1 |> abs) = (x2 - x1 |> abs)
