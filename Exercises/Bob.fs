b0ea0bae229f316b2e1f1fdca70b51640db08c0e
axmty <alexandre.metgy@gmail.com>
2022-04-10 23:47:06 +0200

    Add 3 exercises.

---
+++ Exercises/Bob.fs
@@ -0,0 +1,14 @@
+module Bob
+
+let response (input: string) : string =
+  let input = input.Trim()
+  let hasLetter = input |> String.exists System.Char.IsLetter
+  let yelling = hasLetter && input.ToUpper() = input
+  let asking = input.EndsWith '?'
+
+  match input, yelling, asking with
+  | (input, _, _) when System.String.IsNullOrWhiteSpace input -> "Fine. Be that way!"
+  | (_, true, true) -> "Calm down, I know what I'm doing!"
+  | (_, true, false) -> "Whoa, chill out!"
+  | (_, false, true) -> "Sure."
+  | _ -> "Whatever."
---
+++ Exercises/Clock.fs
@@ -0,0 +1,18 @@
+module Clock
+
+let rec overflow hours minutes =
+  match hours, minutes with
+  | (h, m) when m >= 60 -> overflow (h + 1) (m - 60)
+  | (h, m) when m < 0 -> overflow (h - 1) (m + 60)
+  | (h, m) when h >= 24 -> overflow (h - 24) m
+  | (h, m) when h < 0 -> overflow (h + 24) m
+  | _ -> (hours, minutes)
+
+let create hours minutes = overflow hours minutes
+
+let add minutesToAdd (hours, minutes) = create hours (minutes + minutesToAdd)
+
+let subtract minutesToSubstract (hours, minutes) =
+  create hours (minutes - minutesToSubstract)
+
+let display (hours, minutes) = sprintf "%02d:%02d" hours minutes
