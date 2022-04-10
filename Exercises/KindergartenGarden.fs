6ac0e8af6a64a2c9c96ae2e89203a3544b0c8bb1
axmty <alexandre.metgy@gmail.com>
2022-04-11 00:42:08 +0200

    Add 1 exercise.

---
+++ Exercises/KindergartenGarden.fs
@@ -0,0 +1,33 @@
+module KindergartenGarden
+
+type Plant =
+  | Clover
+  | Grass
+  | Radishes
+  | Violets
+
+let plants (diagram: string) student =
+  let index =
+    [ "Alice"
+      "Bob"
+      "Charlie"
+      "David"
+      "Eve"
+      "Fred"
+      "Ginny"
+      "Harriet"
+      "Ileana"
+      "Joseph"
+      "Kincaid"
+      "Larry" ]
+    |> List.findIndex ((=) student)
+
+  diagram.Split '\n'
+  |> Seq.collect (Seq.skip (index * 2) >> Seq.take 2)
+  |> Seq.map (function
+    | 'C' -> Clover
+    | 'G' -> Grass
+    | 'R' -> Radishes
+    | 'V' -> Violets
+    | _ -> failwith "unknown plant")
+  |> List.ofSeq
